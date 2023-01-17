404724eee2280540b194ef7a0199e1c6df26cba3
hasanfarooqstar <102507210+hasanfarooqstar@users.noreply.github.com>
2023-01-17 17:14:52 +0500

    icon and social links added

--- src/components/section-components/social.js
+++ src/components/section-components/social.js
@@ -10,13 +10,17 @@ class Social extends Component {
       <div className="ltn__social-media">
         <ul>
           <li>
-            <a href="https://www.facebook.com/tuna-theme" title="Facebook">
+            <a href="https://www.facebook.com/asfandyar.khan.355138" title="Facebook">
               <i className="fab fa-facebook-f" />
             </a>
           </li>
-          {/* <li><a href="https://www.twitter.com/tuna-theme" title="Twitter"><i className="fab fa-twitter" /></a></li> */}
           <li>
-            <a href="https://www.instagram.com/tuna-theme" title="Instagram">
+            <a href="https://twitter.com/filestraders" title="Twitter">
+              <i className="fab fa-twitter" />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/noorkhan8755/" title="Instagram">
               <i className="fab fa-instagram" />
             </a>
           </li>
